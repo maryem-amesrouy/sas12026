36d01528424bd93911539d320960f27974efc3f5
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-01 10:36:23 +0200

    message

---
+++ day1/variable/Profil-un-joueur.js
@@ -0,0 +1,11 @@
+let pseudo = "ShadowX";
+let niveaux= 12;
+let age = 21;
+let score = 4580;
+let pays = "Maroc";
+
+console.log(pseudos);
+console.log(niveaux);
+console.log(age);
+console.log(score);
+console.log(pays);
