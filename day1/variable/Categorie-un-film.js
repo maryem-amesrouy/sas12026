dfe54d90262044669c3a27a08aeb8df6de9748f2
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-01 15:40:05 +0200

    categorie un film

--- day1/variable/Categorie-un-film.js
+++ day1/variable/Categorie-un-film.js
@@ -1,2 +1,9 @@
 let prompt = require("prompt-sync")();
-let test = prompt("Entrez un nombre : ");
+let Duree = prompt("Entrez la duree du film : ");
+if(Duree < 60 ){
+    console.log("Court métrage");
+}else if(60 <= Duree && Duree < 120 ){
+    console.log("Film standard");
+}else{
+    console.log("Film long");
+}
