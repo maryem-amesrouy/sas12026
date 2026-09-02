0330db762286a39dff38c066bf109945e5391ae9
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-02 09:52:29 +0200

    les notes final

--- day1/variable/note-finale.js
+++ day1/variable/note-finale.js
@@ -1,7 +1,7 @@
 let prompt = require("prompt-sync")();
-let Controle =Number(prompt("enter Contrôle continu : "));
-let Projet =Number(prompt("enter Projet continu : "));
-let Examen = Number(prompt("enter Examen final : "));
-let noteFinal = (Controle * 2 + Projet * 3 + Examen * 5) / 10;
+let Controle =(prompt("enter Contrôle continu : "));
+let Projet =(prompt("enter Projet continu : "));
+let Examen = (prompt("enter Examen final : "));
+let noteFinal = (+Controle * 2 + +Projet * 3 + +Examen * 5) / 10;
 
 console.log("note final = ", noteFinal);
