9c8081d0abb6bc4c0403dc7bc281e7b77b6556e5
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-02 09:43:24 +0200

    consommation une voiture

--- day1/variable/consommation-une-voiture.js
+++ day1/variable/consommation-une-voiture.js
@@ -1,9 +1,9 @@
 let prompt = require("prompt-sync")();
-let kilomètres = Number(prompt("entrer la distance parcourue en kilomètre:"));
-let litres = Number(prompt("entrer la quantité de carburant consommée en litres:"));
+let kilomètres = (prompt("entrer la distance parcourue en kilomètre:"));
+let litres = (prompt("entrer la quantité de carburant consommée en litres:"));
 let Consommation = (litres / kilomètres) * 100;
 
-console.log("kilomètre:",ilomètres);
+console.log("kilomètre:",kilomètres);
 console.log("litres:", litres);
 console.log("consommation =" , Consommation);
 
