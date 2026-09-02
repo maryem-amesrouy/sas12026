179abb68da724bdc56a4264dc141d57fec0c5458
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-02 14:42:31 +0200

    Accumulation de points

--- day2/boucles/chalenge8.js
+++ day2/boucles/chalenge8.js
@@ -2,4 +2,11 @@ let prompt =require("prompt-sync")();
 
 let mission =prompt("combien de missions est realié : ");
 let scor = 100;
+let mult ;
+let i;
+
+for (let i = 1; i <= mission; i++) {
+    mult = i * scor ;
+  console.log("Mission", i , "→ Score : ", mult);
+}
 
