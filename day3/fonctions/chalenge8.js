a0d332dd14fba5ee174d51e5f27e85445abaddf8
Maryem-Amesrouy <maryemamesrouy@gmail.com>
2026-09-07 10:33:11 +0200

    Convertisseur Universel de Devises avec Callback

---
+++ day3/fonctions/chalenge8.js
@@ -0,0 +1,12 @@
+
+function convertirMontan(montant , taux){
+   return montant * taux
+}
+
+function formatter (){
+    convertirMontan()
+    let resultat = convertirMontan(10 , 6)
+    console.log (resultat)
+}
+
+formatter ()
